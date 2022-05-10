8f0807474380503919f46fbde25452ed2222e0e5
irinakalman <kalman0292@gmail.com>
2022-05-11 02:08:36 +0300

    added button to change the stroke width, button to clear canvas, to save it as png and the button to keep canvas after page reloads by storing it in local storage

--- canvas.js
+++ canvas.js
@@ -45,12 +45,14 @@
 // });
 
 // // to make buttons to change width of stroke and color
+
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 ctx.lineWidth = 2;
 ctx.lineCap = "round";
 ctx.filter = "blur(1px)"; // blur(px)
 ctx.lineJoin = "round"; // round, bevel, miter
+let coordinates = []; // to save mouse clicks to an array
 
 let isDrawing = false;
 x = 0;
@@ -58,7 +60,7 @@ y = 0;
 
 // start drawing
 canvas.addEventListener("mousedown", (e) => {
-  x = e.offsetX;
+  x = e.offsetX; // x coordinate of the mouse
   y = e.offsetY;
   isDrawing = true;
 });
@@ -82,18 +84,24 @@ canvas.addEventListener("mouseup", (e) => {
   }
 });
 
-let myColor2 = 0;
+let myColor2 = 0; // 0 - 360
 
 function drawLine(ctx, x1, y1, x2, y2) {
   if (!isDrawing) return;
   ctx.beginPath();
   ctx.strokeStyle = `hsl(${myColor2}, 100%, 50%, 1.0)`; // 0 - 360, 0 - 100, 0 - 100, 0 - 1
   myColor2 = (myColor2 + 1) % 360; // 0 - 360
-  ctx.moveTo(x1, y1);
-  ctx.lineTo(x2, y2);
-  ctx.stroke();
-  ctx.closePath();
+  ctx.moveTo(x1, y1); // defines the starting point of the line
+  ctx.lineTo(x2, y2); // defines the ending point of the line
+  ctx.stroke(); // draws the line
+  ctx.closePath(); // closes the path after drawing 😋
+  coordinates.push({ x1, y1, x2, y2 }); // saves the coordinates to an array
 }
+
+// Save the default state
+ctx.save(0, 0, canvas.width, canvas.height);
+// Restore the default state
+// ctx.restore(0, 0, canvas.width, canvas.height);
 
 // even listener for buttons
 
@@ -101,6 +109,8 @@ const btn = document.querySelector("#plusWidth");
 const btn2 = document.querySelector("#minusWidth");
 const btn3 = document.querySelector("#clearCanvas");
 const btn4 = document.querySelector("#saveCanvas");
+const btn5 = document.querySelector("#savetoLocalStorage"); // to save canvas to local storage
+const btn6 = document.querySelector("#loadfromLocalStorage"); // reload canvas
 
 btn.addEventListener("click", (e) => {
   ctx.lineWidth = ctx.lineWidth + 1;
@@ -110,14 +120,41 @@ btn2.addEventListener("click", (e) => {
   ctx.lineWidth = ctx.lineWidth - 1;
 });
 btn3.addEventListener("click", (e) => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height); // starts at the upper-left corner and draws a rectangle of the specified width and height.
 });
-btn4.addEventListener("click", (e) => { // save canvas as image
-    const link = document.createElement("a"); // create a link
-    link.download = "canvas.png"; // file name
-    link.href = canvas.toDataURL("image/png"); // base64
-    link.click(); // download
-})
+btn4.addEventListener("click", (e) => {
+  // save canvas as image
+  const link = document.createElement("a"); // create a link
+  link.download = "canvas.png"; // file name
+  link.href = canvas.toDataURL("image/png"); // base64
+  link.click(); // download
+});
+btn5.addEventListener("click", (e) => {
+  // save canvas to local storage
+  localStorage.setItem("canvas", JSON.stringify(coordinates));
+  console.log(localStorage.getItem("canvas")); // to see the array in the console
+});
+btn6.addEventListener("click", (e) => {
+  // reload canvas
+  coordinates = JSON.parse(localStorage.getItem("canvas"));
+  isDrawing = true;
+  coordinates.forEach((coordinate) => {
+    // to draw the array
+    drawLine(ctx, coordinate.x1, coordinate.y1, coordinate.x2, coordinate.y2); // drawLine(ctx, x1, y1, x2, y2);
+  });
+  isDrawing = false;
+});
+
 // btn.addEventListener("click", myFunction(){
 // console.log('Hello World');
 // }); // when button is clicked
+
+// draw a circle
+
+// ctx.beginPath();
+// ctx.arc(65, 40, 30, 0, 2 * Math.PI);
+// ctx.stroke();
+
+// text on canvas
+// ctx.font = "30px Arial";
+// ctx.fillText("Hello World", 10, 50);
